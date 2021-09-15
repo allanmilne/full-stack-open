b7f309c2af1ca0f71c41b26553b8c1881539d2db
allanmilne <amilne.wd@gmail.com>
2021-09-15 22:09:54 +0100

    Add exercise 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,6 +8,23 @@ const Statistics = ({text, feedback, expression}) => {
   )
 }
 
+const Content = ({good, neutral, bad, total, avg, pos}) => {
+    if (total === 0) {
+        return <h3>No feedback given</h3>
+    }
+
+    return (
+        <div>
+            <Statistics text={'Good ='} feedback={good}/>
+            <Statistics text={'Neutral ='} feedback={neutral}/>
+            <Statistics text={'Bad ='} feedback={bad}/>
+            <Statistics text={'All ='} feedback={total}/>
+            <Statistics text={'Average ='} feedback={avg}/>
+            <Statistics text={'Positive ='} feedback={pos} expression={'%'}/>
+        </div>
+    )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -41,17 +58,19 @@ const App = () => {
       <div>
         <h1>Give Feedback</h1>
           <div>
-            <button onClick={handleGood}>good</button>
-            <button onClick={handleNeutral}>neutral</button>
-            <button onClick={handleBad}>bad</button>
+            <button onClick={handleGood}>Good</button>
+            <button onClick={handleNeutral}>Neutral</button>
+            <button onClick={handleBad}>Bad</button>
           </div>
         <h2>Statistics</h2>
-        <Statistics text={'Good ='} feedback={good}/>
-        <Statistics text={'Neutral ='} feedback={neutral}/>
-        <Statistics text={'Bad ='} feedback={bad}/>
-        <Statistics text={'All ='} feedback={total}/>
-        <Statistics text={'Average ='} feedback={averageScore()}/>
-        <Statistics text={'Positive ='} feedback={positiveScore()} expression={'%'}/>
+        <Content
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            avg={averageScore()}
+            pos={positiveScore()}
+        />
       </div>
   )
 }
