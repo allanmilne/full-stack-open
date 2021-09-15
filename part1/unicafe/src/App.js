fced138ce9815121c9ff6b16d812db65253b3ae8
allanmilne <amilne.wd@gmail.com>
2021-09-15 21:50:59 +0100

    Add exercise 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,13 @@
 import React, { useState } from 'react'
 
+const Statistics = ({text, feedback, expression}) => {
+  return (
+      <div>
+        {text} {feedback}{expression}
+      </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
@@ -38,12 +46,12 @@ const App = () => {
             <button onClick={handleBad}>bad</button>
           </div>
         <h2>Statistics</h2>
-          <div>good = {good}</div>
-          <div>neutral = {neutral}</div>
-          <div>bad = {bad}</div>
-          <div>all = {total}</div>
-          <div>average = {averageScore()}</div>
-          <div>positive = {positiveScore()}%</div>
+        <Statistics text={'Good ='} feedback={good}/>
+        <Statistics text={'Neutral ='} feedback={neutral}/>
+        <Statistics text={'Bad ='} feedback={bad}/>
+        <Statistics text={'All ='} feedback={total}/>
+        <Statistics text={'Average ='} feedback={averageScore()}/>
+        <Statistics text={'Positive ='} feedback={positiveScore()} expression={'%'}/>
       </div>
   )
 }
