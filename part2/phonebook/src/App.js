99355f767a379a9ce7510020544bced7680b4227
allanmilne <amilne.wd@gmail.com>
2021-10-03 06:46:26 +0100

    Add exercise 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,86 +1,84 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Contacts from "./components/Contacts";
 
 const App = (props) => {
-    const [newName, setNewName] = useState("");
-    const [newNumber, setNewNumber] = useState("");
-    const [contacts, setContacts] = useState(props.contacts);
-    const [searchTerm, setSearchTerm] = useState("");
+  const [contacts, setContacts] = useState(props.contacts);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const handleNameChange = (event) => {
-        setNewName(event.target.value);
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const addContact = (event) => {
+    event.preventDefault();
+
+    if (contacts.map((contact) => contact.name).includes(newName)) {
+      return window.alert(`${newName} is already added to the phonebook`);
+    }
+
+    const nameObject = {
+      name: newName,
+      number: newNumber,
     };
 
-    const handleNumberChange = (event) => {
-        setNewNumber(event.target.value);
-    };
+    setContacts(contacts.concat(nameObject));
+    setNewName("");
+    setNewNumber("");
+  };
 
-    const addContact = (event) => {
-        event.preventDefault();
+  const handleSearchTermChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
-        if (contacts.map((contact) => contact.name).includes(newName)) {
-            return window.alert(`${newName} is already added to the phonebook`);
-        }
+  const filteredContacts = contacts.filter((contact) => {
+    return contact.name.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1;
+  });
 
-        const nameObject = {
-            name: newName,
-            number: newNumber,
-        };
+  const contactsToShow = searchTerm !== "" ? filteredContacts : contacts;
 
-        setContacts(contacts.concat(nameObject));
-        setNewName('');
-        setNewNumber('');
-    };
-
-    const handleFilteredSearch = (event) => {
-        const query = event.target.value;
-
-        if (query !== "") {
-            const results = contacts.filter((contact) => {
-                return contact.name.toLowerCase().indexOf(query.toLowerCase()) !== -1;
-            });
-            setContacts(results);
-        } else {
-            setContacts(contacts);
-        }
-
-        setSearchTerm(query);
-    };
-
-    return (
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <div>
+        Filter contacts:{" "}
+        <input
+          placeholder={"Filter contacts..."}
+          value={searchTerm}
+          onChange={handleSearchTermChange}
+        />
+      </div>
+      <h2>Add a new Contact</h2>
+      <form onSubmit={addContact}>
         <div>
-            <h2>Phonebook</h2>
-            <div>
-                Filter contacts: <input
-                    placeholder={"Filter contacts..."}
-                    value={searchTerm}
-                    onChange={handleFilteredSearch}
-                />
-            </div>
-            <h2>Add a new Contact</h2>
-            <form onSubmit={addContact}>
-                <div>
-                    Name: <input
-                        placeholder={"Enter a name"}
-                        value={newName}
-                        onChange={handleNameChange}
-                    />
-                </div>
-                <div>
-                    Number: <input
-                        placeholder={"Enter a number"}
-                        value={newNumber}
-                        onChange={handleNumberChange}
-                    />
-                </div>
-                <div>
-                    <button type="submit">add</button>
-                </div>
-            </form>
-            <h2>Numbers</h2>
-            <Contacts contacts={contacts}/>;
+          Name:{" "}
+          <input
+            placeholder={"Enter a name"}
+            value={newName}
+            onChange={handleNameChange}
+          />
         </div>
-    );
+        <div>
+          Number:{" "}
+          <input
+            placeholder={"Enter a number"}
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Contacts contacts={contactsToShow} />
+    </div>
+  );
 };
 
 export default App;
--- part2/phonebook/src/components/Contacts.js
+++ part2/phonebook/src/components/Contacts.js
@@ -1,15 +1,19 @@
 import React from "react";
 
-const Contacts = ({contacts}) => {
-    return (
-        <div>
-            <ul>
-                {contacts.map((contact) => {
-                    return <li key={contact.name}>{contact.name} {contact.number}</li>;
-                })}
-            </ul>
-        </div>
-    )
-}
+const Contacts = ({ contacts }) => {
+  return (
+    <div>
+      <ul>
+        {contacts.map((contact) => {
+          return (
+            <li key={contact.name}>
+              {contact.name} {contact.number}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
-export default Contacts
+export default Contacts;
