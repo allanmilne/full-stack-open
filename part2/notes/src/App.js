3e72a09e55314fd8a6372b15f4e2d14bf4e5577d
allanmilne <amilne.wd@gmail.com>
2021-10-24 20:01:56 +0100

    Add note Importance toggle

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -7,15 +7,13 @@ const App = () => {
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
 
-  const hook = () => {
+  useEffect(() => {
     console.log("effect");
     axios.get("http://localhost:3001/notes").then((response) => {
       console.log("promise fulfilled");
       setNotes(response.data);
     });
-  };
-
-  useEffect(hook, []);
+  }, []);
   console.log("render", notes.length, "notes");
 
   const addNote = (event) => {
@@ -24,20 +22,51 @@ const App = () => {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
-      id: notes.length + 1,
     };
 
-    setNotes(notes.concat(noteObject));
-    setNewNote("");
+    axios.post("http://localhost:3001/notes", noteObject).then((response) => {
+      console.log(response);
+
+      setNotes(notes.concat(noteObject));
+      setNewNote("");
+    });
   };
 
   const handleNoteChange = (event) => {
+    console.log(event.target.value);
     setNewNote(event.target.value);
   };
 
   const notesToShow = showAll
     ? notes
     : notes.filter((notes) => notes.important);
+
+  const toggleImportanceOf = (id) => {
+    const url = `http://localhost:3001/notes/${id}`;
+
+    // loop through notes and find the first element that matches our passed in id and assign it to a variable
+    const note = notes.find((note) => note.id === id);
+
+    // spreading our note object, update the value of important to the opposite of it's previous value
+    const changedNote = { ...note, important: !note.important };
+
+    // Make a PUT request to the server with our change
+    axios.put(url, changedNote).then((response) => {
+      console.log(response);
+
+      // now we want to update the state of notes with the server response
+
+      /*
+       * REMEMBER map is returning a NEW array and not a mutation
+       *
+       * The map method creates a new array by mapping every item from the old array into an item in the new array.
+       * In our example, the new array is created conditionally so that if note.id !== id is true,
+       * we simply copy the item from the old array into the new array.
+       * If the condition is false, then the note object returned by the server is added to the array instead.
+       */
+      setNotes(notes.map((note) => (note.id !== id ? note : response.data)));
+    });
+  };
 
   return (
     <div>
@@ -48,8 +77,12 @@ const App = () => {
         </button>
       </div>
       <ul>
-        {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+        {notesToShow.map((note, index) => (
+          <Note
+            key={index}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
