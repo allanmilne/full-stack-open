e36f52b09b7ad795d8760791465da0eab94f34ea
allanmilne <amilne.wd@gmail.com>
2021-10-27 20:16:29 +0100

    Add exercise 2.16

--- part2/phonebook/src/services/contacts.js
+++ part2/phonebook/src/services/contacts.js
@@ -2,17 +2,23 @@ import axios from "axios";
 
 const baseURL = "http://localhost:3001/contacts";
 
-const getAll = () => {
-  const request = axios.get(baseURL);
-  return request.then((response) => response.data);
-};
-
 const create = (newContactObject) => {
   const request = axios.post(baseURL, newContactObject);
   return request.then((response) => response.data);
 };
 
+const getAll = () => {
+  const request = axios.get(baseURL);
+  return request.then((response) => response.data);
+};
+
+const update = (id, contactObject) => {
+  const request = axios.put(`${baseURL}/${id}`, contactObject);
+  return request.then((response) => response.data);
+};
+
 export default {
+  create,
   getAll,
-  create
-}
+  update,
+};
