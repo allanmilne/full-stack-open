a8c4b93cc4340c65858ddea236f9f1b432469d83
allanmilne <amilne.wd@gmail.com>
2021-11-07 06:07:28 +0000

    Added Footer component with inline styling

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Footer from "./components/Footer";
 import Note from "./components/Note";
 import Notification from "./components/Notification";
 import noteService from "./services/notes";
@@ -96,6 +97,7 @@ const App = () => {
         />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   );
 };
---
+++ part2/notes/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  const footerStyle = {
+    color: "grey",
+    fontStyle: "bold",
+    fontSize: 10,
+  };
+
+  return (
+    <div style={footerStyle}>
+      <br />
+      <br />
+      Note App, 2021
+    </div>
+  );
+};
+
+export default Footer;
