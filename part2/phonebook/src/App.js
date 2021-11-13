04da9a1b4589d50fc432d63d7ebb48a002855540
allanmilne <amilne.wd@gmail.com>
2021-11-13 23:27:47 +0000

    Add exercises 2.19 && 2.20

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import Contacts from "./components/Contacts";
 import Filter from "./components/Filter";
+import Contacts from "./components/Contacts";
 import ContactForm from "./components/ContactForm";
+import Notification from "./components/Notification";
 import contactService from "./services/contacts";
 
 const App = () => {
@@ -9,11 +10,23 @@ const App = () => {
   const [name, setName] = useState("");
   const [number, setNumber] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
+  const [notification, setNotification] = useState(null);
 
   useEffect(() => {
-    contactService.getAll().then((initialContacts) => {
-      setContacts(initialContacts);
-    });
+    contactService
+      .getAll()
+      .then((initialContacts) => {
+        setContacts(initialContacts);
+      })
+      .catch((error) => {
+        setNotification({
+          type: "error",
+          message: error.message,
+        });
+        setTimeout(() => {
+          setNotification(null);
+        }, 5000);
+      });
   }, []);
 
   const handleNameChange = (event) => {
@@ -51,17 +64,43 @@ const App = () => {
   };
 
   const createContact = (contact) => {
-    contactService.create(contact).then((returnedContact) => {
-      setContacts(contacts.concat(returnedContact));
-    });
+    contactService
+      .create(contact)
+      .then((returnedContact) => {
+        setContacts(contacts.concat(returnedContact));
+      })
+      .then(() => {
+        setNotification({
+          type: "success",
+          message: `${contact.name}'s details were added to your phonebook`,
+        });
+        setTimeout(() => {
+          setNotification(null);
+        }, 5000);
+      })
+      .catch((err) => console.error(err.message));
   };
 
   const deleteContact = (contactToDelete) => {
     if (window.confirm(`Delete ${contactToDelete.name}`)) {
-      contactService.deleteContact(contactToDelete.id).then(() => {
-        // remove contactToDelete from state
-        setContacts(contacts.filter((contact) => contact !== contactToDelete));
-      });
+      contactService
+        .deleteContact(contactToDelete.id)
+        .then(() => {
+          // remove contactToDelete from state
+          setContacts(
+            contacts.filter((contact) => contact !== contactToDelete)
+          );
+        })
+        .then(() => {
+          setNotification({
+            type: "warning",
+            message: `${contactToDelete.name}'s details were deleted from your phonebook`,
+          });
+          setTimeout(() => {
+            setNotification(null);
+          }, 5000);
+        })
+        .catch((err) => console.log(err));
     }
   };
 
@@ -74,6 +113,30 @@ const App = () => {
             contact.id !== existingContact.id ? contact : returnedContact
           )
         );
+      })
+      .then(() => {
+        setNotification({
+          type: "warning",
+          message: `${existingContact.name}'s details were updated`,
+        });
+        setTimeout(() => {
+          setNotification(null);
+        }, 5000);
+      })
+      .catch((err) => {
+        if (err.response.status === 404) {
+          setNotification({
+            type: "error",
+            message: `${existingContact.name}'s details have already been removed from the server`,
+          });
+          setTimeout(() => {
+            setNotification(null);
+          }, 5000);
+          // deleted contact gets filtered out from state
+          setContacts(
+            contacts.filter((contact) => contact.id !== existingContact.id)
+          );
+        }
       });
   };
 
@@ -88,6 +151,7 @@ const App = () => {
 
   return (
     <div>
+      <Notification notification={notification} />
       <h2>Phonebook</h2>
       <Filter
         searchTerm={searchTerm}
